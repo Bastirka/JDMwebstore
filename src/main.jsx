e4db134cd33ef8baec4faa1ba6938c7f8a9a235b
Bastirka <140961208+Bastirka@users.noreply.github.com>
2025-09-08 18:05:14 +0300

    Update main.jsx
    kl

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
+// src/main.jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from './App'            // tieši 'App', bez paplašinājuma un ar lielo A
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
